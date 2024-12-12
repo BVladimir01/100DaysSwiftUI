f0084503ce9eef4f2a767389b86a5bdfe99e9125
Vladimir <bizin.vladimir01@gmail.com>
2024-12-12 11:02:16 +0300

    complete day 3 of navigation project

--- Navigation/Navigation/ContentView.swift
+++ Navigation/Navigation/ContentView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct ContentView: View {
     
     @State private var pathStore = PathStore()
+    @State private var title = "List"
     
     var body: some View {
         NavigationStack(path: $pathStore.path) {
@@ -17,6 +18,29 @@ struct ContentView: View {
                 .navigationDestination(for: Int.self) { num in
                     DetaiView(number: num, path: $pathStore.path)
                 }
+//            List(0..<100) {
+//                Text("\($0)")
+//            }
+//            .navigationTitle($title)
+//            .navigationBarTitleDisplayMode(.inline)
+//            .toolbarBackground(.blue, for: .navigationBar)
+//            .toolbarColorScheme(.dark, for: .navigationBar)
+//            .toolbar {
+//                ToolbarItemGroup(placement: .confirmationAction) {
+//                    button
+//                    button
+//                }
+//                ToolbarItemGroup(placement: .secondaryAction) {
+//                    button
+//                    button
+//                }
+//            }
+        }
+    }
+    
+    private var button: some View {
+        Button("somebutton") {
+            
         }
     }
 }
