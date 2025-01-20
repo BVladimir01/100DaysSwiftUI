bcd9d33f40509c76e443fd21d22ded33137c0e02
Vladimir <bizin.vladimir01@gmail.com>
2025-01-20 10:53:24 +0300

    day 56, challenge for bookworm project; improve addBookView, force user to enter title and author

--- Bookworm/Bookworm/AddBookView.swift
+++ Bookworm/Bookworm/AddBookView.swift
@@ -19,35 +19,80 @@ struct AddBookView: View {
     @State private var review = ""
     @State private var rating = 5
     
+    @State private var alertMessage = ""
+    @State private var alertTitle = ""
+    @State private var showingAlert = false
+
+    
     var body: some View {
         NavigationStack {
             Form {
-                Section {
-                    TextField("Name of book", text: $title)
-                    TextField("Author's name", text: $author)
-                    Picker("Genre", selection: $genre) {
-                        ForEach(Book.Genre.allCases, id: \.self) {
-                            Text($0.rawValue.capitalized)
-                        }
-                    }
-                }
-                
-                Section("Write review") {
-                    TextEditor(text: $review)
-                    HStack {
-                        Text("Rating")
-                        Spacer()
-                        RatingView(rating: $rating)
-                    }
-                    }
-                
-                Section {
-                    Button("Save") {
-                        modelContext.insert(Book(title: title, author: author, genre: genre, rating: rating, review: review))
-                        dismiss()
-                    }
+                topSection
+                reviewSection
+                saveSection
+            }
+            .alert(alertTitle, isPresented: $showingAlert) {
+                Button("OK") {}
+            } message: {
+                Text(alertMessage)
+            }
+        }
+    }
+    
+    private var topSection: some View {
+        Section {
+            TextField("Name of book", text: $title)
+            TextField("Author's name", text: $author)
+            Picker("Genre", selection: $genre) {
+                ForEach(Book.Genre.allCases, id: \.self) {
+                    Text($0.rawValue.capitalized)
                 }
             }
+        }
+    }
+    
+    private var reviewSection: some View {
+        Section("Write review") {
+            TextEditor(text: $review)
+            HStack {
+                Text("Rating")
+                Spacer()
+                RatingView(rating: $rating)
+            }
+        }
+    }
+    
+    private var saveSection: some View {
+        Section {
+            HStack {
+                Spacer()
+                Button("Save", action: saveAction)
+                Spacer()
+            }
+        }
+    }
+    
+    private func saveAction() {
+        if bookIsValid() {
+            modelContext.insert(Book(title: title, author: author, genre: genre, rating: rating, review: review))
+            dismiss()
+        } else {
+            determineSavingIssue()
+            showingAlert = true
+        }
+    }
+    
+    private func bookIsValid() -> Bool {
+        return title.isNotEmpty && author.isNotEmpty
+    }
+    
+    private func determineSavingIssue() {
+        if title.isEmpty {
+            alertTitle = "Book's title is empty"
+            alertMessage = "Enter valid name of book"
+        } else {
+            alertTitle = "Author's name is empty"
+            alertMessage = "Enter valid author's name"
         }
     }
 }
---
+++ Bookworm/Bookworm/Extensions.swift
@@ -0,0 +1,15 @@
+//
+//  Extensions.swift
+//  Bookworm
+//
+//  Created by Vladimir on 20.01.2025.
+//
+
+import Foundation
+
+
+extension Collection {
+    var isNotEmpty: Bool {
+        return !self.isEmpty
+    }
+}
