1fe560ca58d2790eb574d2c5ad17c4dad8889ff4
Vladimir <bizin.vladimir01@gmail.com>
2025-03-05 11:15:38 +0300

    day 65, InstaFilter: start building UI, add PhotosPicker, ContentUnavailableView, Slider, Image(with filter) rendering functionality

--- InstaFilter/InstaFilter/ContentView.swift
+++ InstaFilter/InstaFilter/ContentView.swift
@@ -5,50 +5,70 @@
 //  Created by Vladimir on 28.02.2025.
 //
 
+import CoreImage
+import CoreImage.CIFilterBuiltins
 import PhotosUI
 import StoreKit
 import SwiftUI
 
 struct ContentView: View {
     
-    @State var images: [Image] = []
-    @State var imagesChoice: [PhotosPickerItem] = []
-    @Environment(\.requestReview) var requestReview
+    @State private var image: Image?
+    @State private var intensity = 0.5
+    @State private var photoItem: PhotosPickerItem?
+    
+    @State private var currentFilter = CIFilter.sepiaTone()
+    private var context = CIContext()
     
     var body: some View {
-        VStack {
-            ScrollView {
-                ForEach(0..<images.count, id: \.self) { i in
-                    images[i]
-                        .resizable()
-                        .scaledToFit()
-                }
-            }
-            PhotosPicker(selection: $imagesChoice, maxSelectionCount: 3, matching: .images) {
-                Label("Select photos", systemImage: "photo")
-                    .font(.title)
-            }
-            .onChange(of: imagesChoice) {
-                images.removeAll(keepingCapacity: true)
-                Task {
-                    for choice in imagesChoice {
-                        if let image = try await choice.loadTransferable(type: Image.self) {
-                            images.append(image)
-                        }
+        NavigationStack {
+            VStack {
+                Spacer()
+                PhotosPicker(selection: $photoItem) {
+                    if let image {
+                        image
+                            .resizable()
+                            .scaledToFit()
+                    } else {
+                        ContentUnavailableView("No Picture", systemImage: "photo.badge.plus", description: Text("Tap to import a photo"))
                     }
                 }
+                .onChange(of: photoItem, loadPhoto)
+                Spacer()
+                HStack {
+                    Text("Intensity")
+                    Slider(value: $intensity, in: 0...1)
+                        .onChange(of: intensity, applyProcessing)
+                }
+                .padding(.vertical)
+                HStack {
+                    Button("Change filter", action: changeFilter)
+                }
             }
-//            .padding(.bottom)
-            ShareLink(item: Image(.nsx), preview: SharePreview("Share nsx", image: Image(.nsx))) {
-                Label("Share", systemImage: "square.and.arrow.up")
-                    .font(.title)
-                    .padding(.vertical)
-            }
-            Button("Review request") {
-                requestReview()
-            }
-            .font(.title)
+            .padding([.horizontal, .bottom])
+            .navigationTitle("InstaFilter")
         }
+    }
+    
+    private func changeFilter() {
+        
+    }
+    
+    private func loadPhoto() {
+        Task {
+            guard let imageData = try? await photoItem?.loadTransferable(type: Data.self) else { return }
+            guard let beginImage = CIImage(data: imageData) else { return }
+            currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
+            applyProcessing()
+        }
+    }
+    
+    private func applyProcessing() {
+        currentFilter.intensity = Float(intensity)
+        guard let outputImage = currentFilter.outputImage else { return }
+        guard let cgImage = context.createCGImage(outputImage, from: outputImage.extent) else { return }
+        let uiImage = UIImage(cgImage: cgImage)
+        image = Image(uiImage: uiImage)
     }
 }
 
